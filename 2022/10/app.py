f5cfb63b888584fa07e2c7a27544231490f5ce2d
Lázaro Menezes <lazarodm@gmail.com>
2022-12-13 18:49:04 -0300

    Second solution for day 10

--- 2022/10/app.py
+++ 2022/10/app.py
@@ -28,6 +28,58 @@ class App(object):
                 register = action(register)
 
         return sum([signal_store[s] for s in signal_store if s <= 220])
+    
+    def draw_crt(self, program):
+        crt = self.build_crt(program)
+
+        for i, c in enumerate(crt):
+            print(c, end='')
+            if (i + 1) % 40 == 0:
+                print('')
+
+    def build_crt(self, program):
+        crt = ["." for r in range(0,240)]
+
+        self.run_crt(program, crt)
+
+        return crt
+
+    def run_crt(self, path, crt):
+        register = 1
+        cycle = 0
+
+        with open(path, 'r') as file:
+            program = file.read().strip().split("\n")
+
+            for command in program:
+
+                cycles = action = None
+
+                if command == 'noop':
+                   cycles, action = self.noop()
+
+                if command.startswith('addx'):
+                    argument = command.split(' ')[1]
+
+                    cycles, action = self.addx(argument)
+
+                for c in range(cycles):
+                    self.draw_sprite(crt, cycle, register)
+                    cycle += 1
+
+                register = action(register)
+
+    def draw_sprite(self, crt, cycle, register):
+        sprite_size = 3
+        start = register - 1
+        stop = start + sprite_size
+
+        char = '.'
+
+        if cycle % 40 in range(start, stop):
+            char = "#"
+        
+        crt[cycle] = char 
 
     def noop(self):
         return 1, lambda r: r + 0
@@ -42,3 +94,5 @@ class App(object):
 if __name__ == '__main__':
     result = App().sum_register_value("final-input.txt")
     print(result)
+
+    App().draw_crt("final-input.txt")
--- 2022/10/tests.py
+++ 2022/10/tests.py
@@ -11,4 +11,12 @@ class AppTest(unittest.TestCase):
 
         self.assertEqual(13140, total)
 
+    def test_draw_crt(self):
+    
+        app = App()
 
+        crt = app.build_crt("test-input.txt")
+
+        with open("test-output.txt", 'r') as out:
+            expected = out.read().strip().replace("\n", "")
+            self.assertEqual(expected, "".join(crt))
